4d9f725b54b1171071baf02710cb94f43ee5ac1d
linusyu <ilikethisgames@gmail.com>
2014-05-31 01:11:57 +0800

    "user strict"
    "user strict"

--- Tieba Location.user.js
+++ Tieba Location.user.js
@@ -8,38 +8,40 @@
 
 (function(urlHash){
 
-	if(!urlHash) {return;}
-	var hashTarget;
-	
-	function lazyload() {
-		if("undefined" !== typeof unsafeWindow.datalazyload) {
-			unsafeWindow.datalazyload.userConfig.diff = 9999;
-			window.scrollTo(.1,0);	
-			var hash = urlHash.substring(urlHash.indexOf('#')+1);
-			return scrollTo(hash);
-		}
-		setTimeout(lazyload,200);
-	}
-	
-	function scrollTo(hash){    
-		var target = document.body;
-		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-		var observer = new MutationObserver(function(mutations) {
-			mutations.forEach(function() {
-				hashTarget = document.querySelector("a[name='" +hash+ "']");
-				if(hashTarget){
-					observer.disconnect();
-					var prNode = hashTarget.parentNode;
-					prNode.style.fontWeight ="bold";
-					prNode.parentNode.style.display = "block";
-					location.href=location.href;
-				}
-			});
-		});
-		var config = { attributes: true, childList: true, characterData: true };
-		observer.observe(target, config);
-	}
-	
-	lazyload();
-	
+  "user strict";
+
+  if(!urlHash) {return;}
+  var hashTarget;
+  
+  function lazyload() {
+    if("undefined" !== typeof unsafeWindow.datalazyload) {
+      unsafeWindow.datalazyload.userConfig.diff = 9999;
+      window.scrollTo(.1,0);  
+      var hash = urlHash.substring(urlHash.indexOf('#')+1);
+      return scrollTo(hash);
+    }
+    setTimeout(lazyload,200);
+  }
+  
+  function scrollTo(hash){    
+    var target = document.body;
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+    var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function() {
+        hashTarget = document.querySelector("a[name='" +hash+ "']");
+        if(hashTarget){
+          observer.disconnect();
+          var prNode = hashTarget.parentNode;
+          prNode.style.fontWeight ="bold";
+          prNode.parentNode.style.display = "block";
+          location.href=location.href;
+        }
+      });
+    });
+    var config = { attributes: true, childList: true, characterData: true };
+    observer.observe(target, config);
+  }
+  
+  lazyload();
+  
 })(window.location.hash);
