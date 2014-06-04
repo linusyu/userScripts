2a445b99f67f5f75118264021a754ecab11b7aaf
linusyu <ilikethisgames@gmail.com>
2014-06-04 11:41:35 +0800

    DOM 定位

--- Tieba Location.user.js
+++ Tieba Location.user.js
@@ -2,46 +2,59 @@
 // @name        贴吧消息定位
 // @namespace   tieba.baidu.com
 // @include     http://tieba.baidu.com/f?ct=*
-// @version     1
+// @version     2
 // @grant       unsafeWindow
 // ==/UserScript==
 
 (function(urlHash){
 
-  "use strict";
+	"user strict";
 
-  if(!urlHash) {return;}
-  var hashTarget;
-  
-  function lazyload() {
-    if("undefined" !== typeof unsafeWindow.datalazyload) {
-      unsafeWindow.datalazyload.userConfig.diff = 99999;
-      window.scrollTo(.1,0);  
-      var hash = urlHash.substring(urlHash.indexOf('#')+1);
-      return scrollTo(hash);
-    }
-    setTimeout(lazyload,200);
-  }
-  
-  function scrollTo(hash){    
-    var target = document.body;
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-    var observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function() {
-        hashTarget = document.querySelector("a[name='" +hash+ "']");
-        if(hashTarget){
-          observer.disconnect();
-          var prNode = hashTarget.parentNode;
-          prNode.style.fontWeight ="bold";
-          prNode.parentNode.style.display = "block";
-          location.href=location.href;
-        }
-      });
-    });
-    var config = { attributes: true, childList: true, characterData: true };
-    observer.observe(target, config);
-  }
-  
-  lazyload();
+	if(!urlHash) { return; }
+	var hashTarget;
+	var lzl;
+
+	function lazyload() {
+		if("undefined" !== typeof unsafeWindow.datalazyload) {
+			unsafeWindow.datalazyload.userConfig.diff = 99999;
+			window.scrollTo(0,.1);  
+			var hash = urlHash.substring(urlHash.indexOf('#')+1);
+			return scrollTo(hash);
+		}
+		setTimeout(lazyload,200);
+	}
+
+	function scrollTo(hash){    
+		var target = document.body;
+		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+		var observer = new MutationObserver(function(mutations) {
+			mutations.forEach(function() {
+				/*普通楼*/
+				hashTarget = document.querySelector("a[name='" +hash+ "']");
+				if(hashTarget){
+					observer.disconnect();
+					var prNode = hashTarget.parentNode;
+					prNode.style.fontWeight = "bold";
+					gfNode =  prNode.parentNode;
+					gfNode.style.display = "block";
+					return location.href=location.href;
+				}
+				/*楼中楼*/
+				lzl = document.querySelectorAll(".lzl_single_post");
+				[].forEach.call(lzl,function(i){
+					if(JSON.parse(i.getAttribute("data-field").replace(/\'/g,"\"")).spid == hash){
+						observer.disconnect();
+						i.style.fontWeight = "bold";
+						i.parentNode.style.display = "block";
+						return window.scrollTo(0,i.offsetTop - 100);
+					}
+				});
+			});
+		});
+		var config = { attributes: true, childList: true, characterData: true };
+		observer.observe(target, config);
+	}
+
+	lazyload();
   
 })(window.location.hash);
